1c5984c6da3c01cc9afd534efb59c56b72a108ba
Synn <synn@Synns-MacBook-Pro.local>
2026-02-09 22:39:40 +0000

    Overhauled Main Menu

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    [Header("Screens")]
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private GameObject lobbyScreen;
+    [SerializeField] private GameObject roomsListScreen;
+}
