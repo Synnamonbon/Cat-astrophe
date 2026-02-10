36d5552df846d2170dc27a237c6caccb19db9137
Synn <synn@Synns-MacBook-Pro.local>
2026-02-10 13:43:03 +0000

    Added transitions between Login Menu, Title Menu, Lobby menu

--- Assets/Scripts/TitleScreen.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TitleScreen : MonoBehaviour
-{
-    [Header("Screens")]
-    [SerializeField] private GameObject titleScreen;
-    [SerializeField] private GameObject lobbyScreen;
-    [SerializeField] private GameObject roomsListScreen;
-}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,59 @@
+using System;
+using Photon.Pun;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    [Header("Menus")]
+    [SerializeField] private GameObject loginMenu;
+    [SerializeField] private GameObject titleMenu;
+    [SerializeField] private GameObject lobbyMenu;
+
+    [Header("Buttons")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    public void Start()
+    {
+        playButton.interactable = false;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        playButton.interactable = true;
+    }
+
+    public void OnDisplayNameConfirmButton(TMP_InputField displayNameInput)
+    {
+        SessionManager.instance.SetDisplayName(displayNameInput.text);
+
+        SetMenu(titleMenu);
+    }
+
+    public void OnTitleMenuPlayButton()
+    {
+        SetMenu(lobbyMenu);
+
+        lobbyMenu.SetActive(true);
+    }
+
+    public void OpenTitleMenu()
+    {
+        SetMenu(titleMenu);
+
+        quitButton.gameObject.SetActive(true);
+    }
+
+    private void SetMenu(GameObject menu)
+    {
+        loginMenu.SetActive(false);
+        titleMenu.SetActive(false);
+        lobbyMenu.SetActive(false);
+
+        menu.SetActive(true);
+    }
+
+}
