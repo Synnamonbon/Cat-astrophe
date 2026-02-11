1463a8e634e815261a24c14245fcd7b79ae5aabc
alexander-milchev <misho92523@gmail.com>
2026-02-11 18:03:41 +0100

    Login works. TODO: GetPlayerInfo and Set/GetDisplayName

--- Assets/Scripts/Backend/AWSCalls.cs
+++ Assets/Scripts/Backend/AWSCalls.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Text;
 using TMPro;
 using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.Networking;
+// Serialisable classes
+//using PlayerInfo;
+//using DisplayName;          
 
 public class AWSRegister : MonoBehaviour
 {
     public TMP_InputField username_input;
     public TMP_InputField password_input;
+    private string session_token = "";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -62,12 +67,20 @@ public class AWSRegister : MonoBehaviour
         if (request.result == UnityWebRequest.Result.Success)
         {
             Debug.Log("Response: " + request.downloadHandler.text);
+            PlayerInfo response = JsonUtility.FromJson<PlayerInfo>(request.downloadHandler.text);
+            session_token = response.session_token;
         }
         else
         {
             Debug.LogError("Error: " + request.error);
             Debug.Log(request.downloadHandler.text);
         }
+    }
+
+    public IEnumerator GetPlayer()
+    {
+        // TODO implement
+        return null;
     }
 
     public void Register()
