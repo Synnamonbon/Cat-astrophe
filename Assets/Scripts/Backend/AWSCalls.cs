7bab5d23497338011f9943558e871b5c5ee4fe43
Alexander Milchev <alexandermmilchev@gmail.com>
2026-02-15 01:05:37 +0000

    Register, Login, Get and Set Display are implemented. TODO: connect to main code

--- Assets/Scripts/Backend/AWSCalls.cs
+++ Assets/Scripts/Backend/AWSCalls.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 // Serialisable classes
 //using PlayerInfo;
 //using DisplayName;          
@@ -13,20 +14,13 @@ public class AWSRegister : MonoBehaviour
 {
     public TMP_InputField username_input;
     public TMP_InputField password_input;
-    private string session_token = "";
+    public TMP_InputField display_input;
+    public Toggle prefFlag_input;
+    public string session_token = "";
+    public int uid = -1;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Registers a new user into the "registered_users" database
     private IEnumerator RegisterUser(string username, string password)
     {
         string json = $"{{\"username\":\"{username}\", \"password\":\"{password}\"}}";
@@ -51,6 +45,7 @@ public class AWSRegister : MonoBehaviour
         }
     }
 
+    // Generates a bearer token and adds the registered user into the active_users database
     private IEnumerator LoginUser(string username, string password)
     {
         string json = $"{{\"username\":\"{username}\", \"password\":\"{password}\"}}";
@@ -69,6 +64,7 @@ public class AWSRegister : MonoBehaviour
             Debug.Log("Response: " + request.downloadHandler.text);
             PlayerInfo response = JsonUtility.FromJson<PlayerInfo>(request.downloadHandler.text);
             session_token = response.session_token;
+            uid = int.Parse(response.uid);
         }
         else
         {
@@ -77,10 +73,88 @@ public class AWSRegister : MonoBehaviour
         }
     }
 
-    public IEnumerator GetPlayer()
+    // Logs a new guest user into the active_users and generates a bearer token for the session.
+    private IEnumerator GuestLogin(string display_name)
+    {
+        // TODO: implement
+        string url = AWSConfig.GetLoginURL();
+        return null;
+    }
+
+    // Returns own preferred display name or active display name
+    private IEnumerator GetUserDisplayName(bool prefFlag)
     {
         // TODO implement
-        return null;
+        string json = $"{{\"uid\":\"{uid}\"}}";
+        string url;
+        if(prefFlag)
+        {
+            url = AWSConfig.GetPreferredNameURL(uid);
+        } 
+        else
+        {
+            url = AWSConfig.GetDisplayNameURL(uid);
+        }
+
+        UnityWebRequest request = new UnityWebRequest(url, "GET");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Authorization", $"Bearer {session_token}");
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Response: " + request.downloadHandler.text);
+            DisplayName response = JsonUtility.FromJson<DisplayName>(request.downloadHandler.text);
+            Debug.Log("Display name retreived: " + response.display_name);
+        }
+        else
+        {
+            Debug.Log("Error: " + request.error);
+            Debug.Log(request.downloadHandler.text);
+            Debug.Log(url);
+        }
+    }
+
+    // Sets own preferred display name or active display name 
+    private IEnumerator SetUserDisplayName(bool prefFlag, string disp_name)
+    {
+        string json = $"{{\"display_name\":\"{disp_name}\", \"uid\":\"{uid}\"}}";
+        string url;
+        if(prefFlag)
+        {
+            url = AWSConfig.SetPreferredNameURL(uid);
+        } 
+        else
+        {
+            url = AWSConfig.SetDisplayNameURL(uid);
+        }
+        
+        UnityWebRequest request = new UnityWebRequest(url, "PUT");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Authorization", $"Bearer {session_token}");
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        //string jsonBody = System.Text.Encoding.UTF8.GetString(request.uploadHandler.data);
+        //Debug.Log(jsonBody);
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Response: " + request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Error: " + request.error);
+            Debug.Log(request.downloadHandler.text);
+            Debug.Log(url);
+        }
     }
 
     public void Register()
@@ -95,5 +169,18 @@ public class AWSRegister : MonoBehaviour
         string username = username_input.text;
         string password = password_input.text;
         StartCoroutine(LoginUser(username, password));
+    }
+
+    public void GetDisplayName()
+    {
+        bool prefFlag = prefFlag_input.isOn;
+        StartCoroutine(GetUserDisplayName(prefFlag));
+    }
+
+    public void SetDisplayName()
+    {
+        string name = display_input.text;
+        bool prefFlag = prefFlag_input.isOn;
+        StartCoroutine(SetUserDisplayName(prefFlag, name));
     }
 }
--- Assets/Scripts/Backend/AWSConfig.cs
+++ Assets/Scripts/Backend/AWSConfig.cs
@@ -5,5 +5,8 @@ public static class AWSConfig
     private const string BASE_URL = "https://njlqthr3ac.execute-api.eu-north-1.amazonaws.com";
     public static string GetRegisterURL() => $"{BASE_URL}/register";
     public static string GetLoginURL() => $"{BASE_URL}/login";
-    public static string GetPlayerURL(int uid) => $"{BASE_URL}/get/{uid}";
+    public static string GetDisplayNameURL(int uid) => $"{BASE_URL}/get/display_name/{uid}";
+    public static string GetPreferredNameURL(int uid) => $"{BASE_URL}/get/pref_name/{uid}";
+    public static string SetDisplayNameURL(int uid) => $"{BASE_URL}/set/display_name/{uid}";
+    public static string SetPreferredNameURL(int uid) => $"{BASE_URL}/set/pref_name/{uid}";
 }
