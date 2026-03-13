665ae57cfdd85b07b98c3308bf60c5dff8422e8c
alexander-milchev <misho92523@gmail.com>
2026-03-13 15:37:26 +0100

    Starting work on Vision/Sound detect

---
+++ Assets/Scripts/NPCs/EnemyNPCDetection.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyNPCDetection : MonoBehaviour
+{
+    public GameObject visionOrigin;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
