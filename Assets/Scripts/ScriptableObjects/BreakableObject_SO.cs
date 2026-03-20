95400b825eacac7ba863d8b4c5a70eb7336ef919
Synnamonbon <arigator.t@gmail.com>
2026-03-20 13:40:11 +0000

    Organised scenes, reset sync changes, syncing was performing poorly due to uni internet, after testing in better conditions original sync preformed better

---
+++ Assets/Scripts/ScriptableObjects/BreakableObject_SO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BreakableObject_SO", menuName = "Scriptable Objects/BreakableObject_SO")]
+public class BreakableObject_SO : ScriptableObject
+{
+    
+}
