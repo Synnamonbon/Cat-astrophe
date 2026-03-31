cba4d054192f2fd821f22a663143938bb4e6a50f
Synnamonbon <arigator.t@gmail.com>
2026-03-31 22:41:07 +0100

    fixed error

--- Assets/Scripts/AlertManager.cs
+++ Assets/Scripts/AlertManager.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Photon.Pun;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class AlertManager : MonoBehaviourPunCallbacks
